ab14670003fa6faae2a09d80a4c259770e145489
Anandshakya6398 <anandshakya6398@gmail.com>
2026-04-06 19:47:45 +0530

    code for state management

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import { transactionsData } from "../data/mockData";
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [transactions, setTransactions] = useState(transactionsData);
+  const [role, setRole] = useState("viewer");
+  const [search, setSearch] = useState("");
+
+  return (
+    <AppContext.Provider
+      value={{
+        transactions,
+        setTransactions,
+        role,
+        setRole,
+        search,
+        setSearch,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
